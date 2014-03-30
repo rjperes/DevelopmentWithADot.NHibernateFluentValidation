b0b34767c92355089c3c27af09fe1f0b06527e04
Ricardo Peres <rjperes@hotmail.com>
2014-03-30 10:31:08 +0100

    Improvements

--- DevelopmentWithADot.NHibernateFluentValidation/FluentValidation.cs
+++ DevelopmentWithADot.NHibernateFluentValidation/FluentValidation.cs
@@ -8,15 +8,24 @@ namespace DevelopmentWithADot.NHibernateFluentValidation
 	public sealed class FluentValidation
 	{
 		private readonly IDictionary<Type, Dictionary<Delegate, String>> conditions = new ConcurrentDictionary<Type, Dictionary<Delegate, String>>();
-		
-		public FluentValidation Clear<T>()
+
+		internal FluentValidation()
+		{
+			this.Enabled = true;
+		}
+
+		public Boolean Enabled
+		{
+			get;
+			set;
+		}
+
+		public void Clear<T>()
 		{
 			foreach (var type in this.conditions.Where(x => typeof(T).IsAssignableFrom(x.Key)).Select(x => x.Key))
 			{
 				this.conditions.Remove(type);
 			}
-			
-			return (this);
 		}
 
 		public FluentValidation Entity<T>(Func<T, Boolean> condition, String message)
@@ -33,17 +42,20 @@ namespace DevelopmentWithADot.NHibernateFluentValidation
 
 		public void Validate(Object entity)
 		{
-			var applicableConditions = this.conditions.Where(x => entity.GetType().IsAssignableFrom(x.Key)).Select(x => x.Value);
-
-			foreach (var applicableCondition in applicableConditions)
+			if (this.Enabled == true)
 			{
-				foreach (var condition in applicableCondition)
-				{
-					var del = condition.Key;
+				var applicableConditions = this.conditions.Where(x => entity.GetType().IsAssignableFrom(x.Key)).Select(x => x.Value);
 
-					if (Object.Equals(del.DynamicInvoke(entity), false) == true)
+				foreach (var applicableCondition in applicableConditions)
+				{
+					foreach (var condition in applicableCondition)
 					{
-						throw (new ValidationException(entity, condition.Value));
+						var del = condition.Key;
+
+						if (Object.Equals(del.DynamicInvoke(entity), false) == true)
+						{
+							throw (new ValidationException(entity, condition.Value));
+						}
 					}
 				}
 			}
